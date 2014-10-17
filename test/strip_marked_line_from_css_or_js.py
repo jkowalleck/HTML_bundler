4dabaf74426a83b4a1d13faae2d7d0a56a23f2c9
Jan Kowalleck <jan.kowalleck@gmail.com>
2014-10-17 20:05:54 +0200

    stand

--- test/strip_marked_line_from_css_or_js.py
+++ test/strip_marked_line_from_css_or_js.py
@@ -1,1 +1,19 @@
 """ unit test for Bundler.strip_marked_line_from_css_or_js """
+
+
+import unittest
+
+
+from bundler import Bundler
+
+
+class Test_strip_marked_line_from_css_or_js(unittest.TestCase):
+
+    def test_none(self):
+        string = "nothing to change here"
+        self.assertEqual(string, Bundler.strip_marked_line_from_css_or_js(string, ""))
+
+
+    def test_single(self):
+        string = "this should be empty // @strip"
+        self.assertEqual("", Bundler.strip_marked_line_from_css_or_js(string, "strip"))
