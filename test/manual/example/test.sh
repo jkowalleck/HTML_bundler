bbbbcd6b568a1788b3946c31552c1b99ce651af4
Jan Kowalleck <jan.kowalleck@gmail.com>
2014-11-11 02:02:24 +0100

    finished manual test

---
+++ test/manual/example/test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+dir=$(dirname $0)
+tmp=$(mktemp.exe -d)
+
+bundler="$dir/../../../bundler.py --path-dir $dir -i $dir/in.html"
+
+#echo "dir = $dir"
+#echo "tmp = $tmp"
+
+$bundler -o "$tmp/out_raw.html"
+$bundler -o "$tmp/out_fullfeatured.html" --compress --strip-markers "@stripOnBundle" --strip-tags "@stripOnBundle" --strip-comments
+
+errors=0
+
+echo -n "checking out_raw.html ... "
+if diff $tmp/out_raw.html $tmp/out_raw.html >/dev/null
+then
+    echo "same"
+else
+    errors=$errors+1
+    echo "different"
+fi
+
+echo -n "checking out_fullfeatured.html ... "
+if diff $tmp/out_fullfeatured.html $tmp/out_fullfeatured.html >/dev/null
+then
+    echo "same"
+else
+    errors=$errors+2
+    echo "different"
+fi
+
+rm -rf $tmp
+
+echo "exit with errors: $errors"
+exit $errors
